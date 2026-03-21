f29b3b20f65e92373c004a9fa5fc65d47310b671
Joel Miller <millerjoel7597@gmail.com>
2026-03-21 17:26:44 +0100

    wip

--- components/ChickenEssence.tsx
+++ components/ChickenEssence.tsx
@@ -13,8 +13,7 @@ export default function ChickenEssence() {
         backgroundPosition: 'center',
       }}
     >
-      {/* Intersect Borders (Red Ribbons) */}
-      {/* Matching section 3 to blend: top-0 means it starts right where section 3 ends */}
+      {/* Intersect Borders */}
       <div className="absolute top-0 bottom-0 -left-[49px] md:-left-8 w-[80px] md:w-[69px] z-30 pointer-events-none overflow-hidden">
         <Image src="/assets/Intersect-left-2.svg" alt="" fill className="object-cover object-top" />
       </div>
@@ -22,15 +21,31 @@ export default function ChickenEssence() {
         <Image src="/assets/Intersect-right-2.svg" alt="" fill className="object-cover object-top" />
       </div>
 
-      <div className="w-full px-6 md:px-20 relative z-20 flex flex-col items-center">
-        {/* Header - Large Red Title, Single Line, Centered */}
-        <div className="w-full text-center flex justify-center">
+      <div className="w-full relative z-20 flex flex-col items-center">
+        {/* Header */}
+        <div className="w-full text-center flex justify-center mb-4 md:mb-6">
           <h2 className="text-2xl sm:text-4xl md:text-6xl lg:text-[80px] xl:text-[100px] leading-none drop-shadow-sm uppercase whitespace-nowrap">
             real chicken essence
           </h2>
         </div>
 
-        {/* Products Row removed as requested */}
+        {/* Products Row */}
+        <div className="flex flex-col md:flex-row items-center justify-center gap-0 w-full px-4 md:px-8">
+          <Image 
+            src="/assets/Tasty Chicken 1.svg" 
+            alt="Tasty Chicken" 
+            width={982}
+            height={915}
+            className="w-[340px] md:w-[580px] h-auto"
+          />
+          <Image 
+            src="/assets/Rich Spices 1.svg" 
+            alt="Rich Spices" 
+            width={885}
+            height={764}
+            className="w-[340px] md:w-[580px] h-auto"
+          />
+        </div>
       </div>
     </section>
   );
